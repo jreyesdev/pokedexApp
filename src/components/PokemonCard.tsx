c9e5c0e2afedfab106c986f8019db18e22d7e836
Jesus Reyes <djrm1204@gmail.com>
2022-02-23 01:01:27 -0400

    Componente de tarjeta para pokemones

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,116 @@
+import React, {memo, useEffect, useRef, useState} from 'react';
+import {
+  Dimensions,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import ImageColors from 'react-native-image-colors';
+
+import {SimplePokemon} from '../interfaces';
+import FadeInImage from './FadeInImage';
+
+interface Props {
+  pokemon: SimplePokemon;
+}
+
+const windowWidth = Dimensions.get('window').width;
+
+const PokemonCard = ({pokemon}: Props) => {
+  const [bgColor, setBgColor] = useState('grey');
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    ImageColors.getColors(pokemon.picture, {
+      fallback: 'grey',
+      cache: true,
+    })
+      .then(colors => {
+        if (isMounted.current) {
+          switch (colors.platform) {
+            case 'android':
+              setBgColor(colors.dominant || 'grey');
+              break;
+            case 'ios':
+              setBgColor(colors.background || 'grey');
+              break;
+            default:
+              setBgColor('grey');
+              break;
+          }
+        }
+      })
+      .catch(() => setBgColor('#eee'));
+
+    return () => {
+      isMounted.current = false;
+      abortController.abort();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <TouchableOpacity activeOpacity={0.8} key={pokemon.id}>
+      <View style={{...s.card, backgroundColor: bgColor}}>
+        <View style={s.cardContainer}>
+          <Text style={s.name}>{pokemon.name + '\n#' + pokemon.id}</Text>
+          <Image
+            source={require('../assets/pokebola-blanca.png')}
+            style={s.imgPokebola}
+          />
+        </View>
+        <FadeInImage uri={pokemon.picture} style={s.imgPokemon} />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const s = StyleSheet.create({
+  card: {
+    height: 120,
+    width: windowWidth * 0.4,
+    marginHorizontal: 10,
+    marginBottom: 25,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+  cardContainer: {
+    flex: 1,
+    padding: 10,
+    overflow: 'hidden',
+  },
+  name: {
+    fontWeight: '700',
+    fontSize: 20,
+    color: 'white',
+  },
+  imgPokebola: {
+    height: 100,
+    width: 100,
+    position: 'absolute',
+    bottom: -20,
+    right: -20,
+    opacity: 0.3,
+  },
+  imgPokemon: {
+    height: 120,
+    width: 120,
+    position: 'absolute',
+    right: -8,
+    bottom: -5,
+  },
+});
+
+export default memo(PokemonCard);
