d878128037c2e30e6b67fd84c3898b0c06e5588b
Jesus Reyes <djrm1204@gmail.com>
2022-02-23 01:01:51 -0400

    Pantalla principal. Lista de pokemones

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,39 +4,53 @@ import {
   FlatList,
   Image,
   StyleSheet,
+  Text,
   View,
 } from 'react-native';
-import FadeInImage from '../components/FadeInImage';
+import PokemonCard from '../components/PokemonCard';
 import usePokemonPaginated from '../hooks/usePokemonPaginated';
 import {globalStyles} from '../theme/appTheme';
+
+const HeaderTitle = () => <Text style={s.title}>Pokedex</Text>;
 
 const HomeScreen = () => {
   const {loadPokemons, simplePokemonList} = usePokemonPaginated();
 
+  // const onEndReached = () => {
+  //   if (!isLoading) {
+  //     loadPokemons();
+  //   }
+  // };
+
   return (
-    <View style={globalStyles.container}>
+    <View style={globalStyles.containerFull}>
       <Image
         source={require('../assets/pokebola.png')}
         style={globalStyles.imgPokebola}
       />
-      <FlatList
-        data={simplePokemonList}
-        showsVerticalScrollIndicator={false}
-        keyExtractor={poke => poke.id}
-        renderItem={({item}) => (
-          <FadeInImage uri={item.picture} style={s.img} />
-        )}
-        onEndReached={loadPokemons}
-        onEndReachedThreshold={0.3}
-        ListFooterComponent={
-          <ActivityIndicator style={s.load} size={30} color="grey" />
-        }
-      />
+      <View style={s.contList}>
+        <FlatList
+          data={simplePokemonList}
+          numColumns={2}
+          showsVerticalScrollIndicator={false}
+          keyExtractor={poke => poke.id}
+          ListHeaderComponent={<HeaderTitle />}
+          renderItem={({item}) => <PokemonCard pokemon={item} />}
+          onEndReached={loadPokemons}
+          onEndReachedThreshold={0.4}
+          ListFooterComponent={
+            <ActivityIndicator style={s.load} size={30} color="grey" />
+          }
+        />
+      </View>
     </View>
   );
 };
 
 const s = StyleSheet.create({
+  contList: {
+    alignItems: 'center',
+  },
   title: {
     ...globalStyles.title,
     marginVertical: 20,
