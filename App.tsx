7cc7301b4e12bcfe35d0a1c5c0fea7255b383f01
Jesus Reyes <djrm1204@gmail.com>
2022-02-25 00:48:33 -0400

    Agregado Tab navigator

--- App.tsx
+++ App.tsx
@@ -2,13 +2,13 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {StatusBar} from 'react-native';
-import StackNavigator from './src/navigator/StackNavigator';
+import Tabs from './src/navigator/Tabs';
 
 const App = () => {
   return (
     <NavigationContainer>
       <StatusBar barStyle="dark-content" backgroundColor="white" />
-      <StackNavigator />
+      <Tabs />
     </NavigationContainer>
   );
 };
